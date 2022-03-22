1619232cfb695aa95a36f8f429f3e5c5bf30a552
Diligenttechnologies123 <diligenttechnologiespk@gmail.com>
2022-03-22 11:40:34 -0600

    all pages added

---
+++ src/Components/ComingSoon/ComingSoon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CommingSoon = () => {
+  return (
+    <div className='coming-soon'>
+        <h1>
+            Comming Soon
+        </h1>
+    </div>
+  )
+}
---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from './Components/Header/Header';
+import { CommingSoon } from './Components/ComingSoon/ComingSoon';
+
+export const Routes = () => {
+  return (
+    <div>
+        <Header />
+        <CommingSoon />
+    </div>
+  )
+}

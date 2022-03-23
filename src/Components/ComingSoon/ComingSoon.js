59b1c934f08a83f5cfa9af2c7dedfc8d677e8567
Diligenttechnologies123 <diligenttechnologiespk@gmail.com>
2022-03-23 07:48:33 -0600

    remove second logo

--- src/Components/ComingSoon/ComingSoon.js
+++ src/Components/ComingSoon/ComingSoon.js
@@ -4,15 +4,11 @@ import logo from '../../assets/images/diligent-logo.png'
 export const CommingSoon = () => {
   return (
     <div className='coming-soon'>
-  <div className='coming'>
-
-  <div className='diligent-logo'>
-    <img src={logo}></img>
-        </div>
+ 
         <h1>
             Comming Soon...
         </h1>
-  </div>
+
     </div>
   )
 }
